30c8b33df105c1823471c0e0cb733eb7143c3534
Peter Sellars <psellars@gmail.com>
2020-05-19 15:28:13 +1200

    updated account non-prod to 0.12 terraform

--- accounts/non-prod/main.tf
+++ accounts/non-prod/main.tf
@@ -3,16 +3,16 @@ terraform {
 }
 
 data "aws_caller_identity" "current" {
-  provider = "aws.noassume"
+  provider = aws.noassume
 }
 
 data "terraform_remote_state" "organization" {
   backend = "s3"
 
   config {
-    bucket   = "${var.terraform_state_bucket}"
+    bucket   = var.terraform_state_bucket
     key      = "master/organization/terraform.tfstate"
-    region   = "${var.terraform_state_bucket_region}"
+    region   = var.terraform_state_bucket_region
     role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TerragruntReader"
   }
 }
@@ -21,16 +21,16 @@ data "terraform_remote_state" "infosec" {
   backend = "s3"
 
   config {
-    bucket   = "${var.terraform_state_bucket}"
+    bucket   = var.terraform_state_bucket
     key      = "accounts/infosec/terraform.tfstate"
-    region   = "${var.terraform_state_bucket_region}"
+    region   = var.terraform_state_bucket_region
     role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TerragruntReader"
   }
 }
 
 provider "aws" {
   alias  = "noassume"
-  region = "${var.aws_default_region}"
+  region = var.aws_default_region
 }
 
 provider "aws" {
@@ -38,7 +38,7 @@ provider "aws" {
     role_arn = "arn:aws:iam::${data.terraform_remote_state.organization.non_prod_acct_id}:role/Administrator"
   }
 
-  region = "${var.aws_default_region}"
+  region = var.aws_default_region
 }
 
 resource "aws_iam_account_alias" "alias" {
@@ -48,14 +48,15 @@ resource "aws_iam_account_alias" "alias" {
 resource "aws_cloudtrail" "cloudtrail" {
   name                       = "cloudtrail-non-prod"
   s3_key_prefix              = "non-prod"
-  s3_bucket_name             = "${data.terraform_remote_state.infosec.cloudtrail_bucket_id}"
+  s3_bucket_name             = data.terraform_remote_state.infosec.cloudtrail_bucket_id
   enable_log_file_validation = true
   is_multi_region_trail      = true
 }
 
 module "cross_account_role_developer" {
-  source                  = "../../modules/cross-account-role"
-  assume_role_policy_json = "${data.terraform_remote_state.organization.crossaccount_assume_from_infosec_policy_json}"
-  role                    = "${var.developer_role_name}"
-  role_policy_arn         = "${var.developer_default_arn}"
+  source                    = "../../modules/cross-account-role"
+  assume_role_policy_json   = data.terraform_remote_state.organization.crossaccount_assume_from_infosec_policy_json
+  role                      = var.developer_role_name
+  role_policy_arn           = var.developer_default_arn
+  role_max_session_duration = 28800
 }
